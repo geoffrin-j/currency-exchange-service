961068e882ab61ee0c3286ad9674886bd30c61fe
geoffrin-j <geoffrin.j.11@gmail.com>
2024-03-08 14:50:18 +0530

    Data added and properties set accordingly

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values ('1001','USD','INR',60,'');
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values ('1002','EUR','INR',50,'');
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values ('1003','AUD','INR',40,'');
